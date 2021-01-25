594c25ac10277641c68936729bc085569f183b34
t.volchenko <t.volchenko@just-ai.com>
2021-01-26 01:10:04 +0300

    Added authentication

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
-from flask import Flask, flash, request, redirect, url_for, send_from_directory
+import os
+from functools import wraps
+
+import ffmpeg
+
+from flask import Flask, flash, request, redirect, url_for, send_from_directory, jsonify
 from celery import Celery
 from werkzeug.utils import secure_filename
 from uuid import uuid4
-import os
-import ffmpeg
 
 app = Flask(__name__)
 app.config.from_object("config")
@@ -25,7 +28,20 @@ def convert(self, filename, scale='1280x720', codec='h264', bitrate="1500k", pro
         .run()
 
 
+def auth(f):
+    @wraps(f)
+    def result_function(*args, **kwargs):
+        headers = request.headers
+        auth = headers.get("X-Api-Key")
+        if auth == app.config['AUTH_KEY']:
+            return f(*args, **kwargs)
+        else:
+            return jsonify({"message": "ERROR: Unauthorized"}), 401
+    return result_function
+
+
 @app.route('/upload_file', methods=['GET','POST'])
+@auth
 def upload():
     if request.method == 'POST':
         scale = request.form.get("scale", "1280x720")
@@ -50,6 +66,7 @@ def upload():
 
 
 @app.route('/download/<task_id>')
+@auth
 def download(task_id):
     state = client.AsyncResult(task_id).state
     if state == "SUCCESS":
--- config.py
+++ config.py
@@ -4,3 +4,4 @@ CELERY_BROKER_URL = 'redis://localhost:6379/0'
 CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
 
 UPLOAD_FOLDER = '/tmp/uploads'
+AUTH_KEY='123321'
